d8108d1c2828a77d7d8d4abc74bf74ede1c46ee0
jgolde <jonas.golde@gmail.com>
2022-05-09 15:22:33 +0200

    remove check min version

--- template.py
+++ template.py
@@ -47,9 +47,6 @@ from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
 
 
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.19.0.dev0")
-
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/token-classification/requirements.txt")
 
 logger = logging.getLogger(__name__)
