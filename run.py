8ca0fc4b8654fc7ad44468d38bea80b88e1c871c
jgolde <jonas.golde@gmail.com>
2022-05-09 11:50:59 +0200

    remove separate metrics file

--- run.py
+++ run.py
@@ -1,14 +1,38 @@
 import argparse
 
-import torch
 from transformers import AutoConfig, AutoTokenizer
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForTokenClassification
 from datasets import load_dataset
 
+import torch
+import numpy as np
+from seqeval.metrics import classification_report, f1_score
+
 from model import TARSTagger
 from data import tokenize_and_align_labels
-from metric import compute_metrics
+
+def align_predictions(predictions, label_ids):
+    preds = np.argmax(predictions, axis=2)
+    batch_size, seq_len = preds.shape
+    labels_list = preds_list = [], []
+
+    for batch_idx in range(batch_size):
+        example_labels, example_preds = [], []
+        for seq_idx in range(seq_len):
+            if label_ids[batch_idx, seq_idx] != -100:
+                example_labels.append(index2tag[label_ids[batch_idx][seq_idx]])
+                example_preds.append(index2tag[preds[batch_idx][seq_idx]])
+
+        labels_list.append(example_labels)
+        preds_list.append(example_preds)
+
+    return preds_list, labels_list
+
+def compute_metrics(eval_pred):
+    y_pred, y_true = align_predictions(eval_pred.predictions, eval_pred.label_ids)
+    return {"classification_report": classification_report(y_true, y_pred),
+            "f1": f1_score(y_true, y_pred)}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
