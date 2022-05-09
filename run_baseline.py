eba47f43742cb145b5d109178dfac8420f201a2d
jgolde <jonas.golde@gmail.com>
2022-05-09 17:11:51 +0200

    remove check min version

--- run_baseline.py
+++ run_baseline.py
@@ -1,67 +1,32 @@
 import argparse
-import logging
 
-import transformers
-from transformers import AutoConfig, AutoTokenizer
+from transformers import AutoConfig, AutoTokenizer, AutoModel
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForTokenClassification
 
-import datasets
 from datasets import load_dataset
 
 import torch
 import numpy as np
 from seqeval.metrics import classification_report, f1_score
 
-from model import TARSTagger
 from data import tokenize_and_align_labels
 
-logger = logging.getLogger(__name__)
-
-
-def align_predictions(predictions, label_ids):
-    preds = np.argmax(predictions, axis=2)
-    batch_size, seq_len = preds.shape
-    labels_list, preds_list = [], []
-
-    for batch_idx in range(batch_size):
-        example_labels, example_preds = [], []
-        for seq_idx in range(seq_len):
-            if label_ids[batch_idx, seq_idx] != -100:
-                example_labels.append(index2tag[label_ids[batch_idx][seq_idx]])
-                example_preds.append(index2tag[preds[batch_idx][seq_idx]])
-
-        labels_list.append(example_labels)
-        preds_list.append(example_preds)
-
-    return preds_list, labels_list
-
-def compute_metrics(eval_pred):
-    y_pred, y_true = align_predictions(eval_pred.predictions, eval_pred.label_ids)
-    return {"classification_report": classification_report(y_true, y_pred),
-            "f1": f1_score(y_true, y_pred)}
-
-def get_f1_score(trainer, dataset):
-    return trainer.predict(dataset).metrics["test_f1"]
 
 def main():
+    # parser training arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="xlm-roberta-large")
+    parser.add_argument("--output_dir", type=str)
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--lr", type=float, default=5e-6)
     parser.add_argument("--epochs", type=int, default=10)
-
     args = parser.parse_args()
 
-    log_level = logging.INFO
-    logger.setLevel(log_level)
-    datasets.utils.logging.set_verbosity(log_level)
-    transformers.utils.logging.set_verbosity(log_level)
-    transformers.utils.logging.enable_default_handler()
-    transformers.utils.logging.enable_explicit_format()
-
+    # set cuda device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    # load dataset
     dataset = load_dataset("conll2003")
     tags = dataset["train"].features["ner_tags"].feature
     index2tag = {idx: tag for idx, tag in enumerate(tags.names)}
@@ -70,26 +35,43 @@ def main():
     config = AutoConfig.from_pretrained(args.model, num_labels=tags.num_classes,
                                         id2label=index2tag, label2id=tag2index)
 
-    model = TARSTagger.from_pretrained(args.model, config=config).to(device)
+    model = AutoModel.from_pretrained(args.model, config=config).to(device)
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
 
     tokenized_dataset = dataset.map(lambda p: tokenize_and_align_labels(p, tokenizer), batched=True,
                                     remove_columns=["tokens", "pos_tags", "chunk_tags", "ner_tags"])
 
-    logging_steps = len(tokenized_dataset["train"]) // args.batch_size
-
-    path = "resouces/tars/run1/"
     training_arguments = TrainingArguments(
-        output_dir="resouces/tars/run1",
+        output_dir=args.output_dir,
         evaluation_strategy="epoch",
         learning_rate=args.lr,
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
         num_train_epochs=args.epochs,
-        logging_steps=logging_steps,
-        weight_decay=0.01
     )
+
+    def align_predictions(predictions, label_ids):
+        preds = np.argmax(predictions, axis=2)
+        batch_size, seq_len = preds.shape
+        labels_list, preds_list = [], []
+
+        for batch_idx in range(batch_size):
+            example_labels, example_preds = [], []
+            for seq_idx in range(seq_len):
+                if label_ids[batch_idx, seq_idx] != -100:
+                    example_labels.append(index2tag[label_ids[batch_idx][seq_idx]])
+                    example_preds.append(index2tag[preds[batch_idx][seq_idx]])
+
+            labels_list.append(example_labels)
+            preds_list.append(example_preds)
+
+        return preds_list, labels_list
+
+    def compute_metrics(eval_pred):
+        y_pred, y_true = align_predictions(eval_pred.predictions, eval_pred.label_ids)
+        return {"classification_report": classification_report(y_true, y_pred),
+                "f1": f1_score(y_true, y_pred)}
 
     trainer = Trainer(
         model=model,
@@ -101,9 +83,24 @@ def main():
         compute_metrics=compute_metrics
     )
 
-    trainer.train()
+    train_result = trainer.train()
+    metrics = train_result.metrics
+    trainer.save_model()
 
-    trainer.save_model(f"{path}/final")
+    metrics["train_samples"] = len(dataset["train"])
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+
+    metrics = trainer.evaluate()
+    metrics["eval_samples"] = len(dataset["validation"])
+    trainer.log_metrics("eval", metrics)
+    trainer.save_metrics("eval", metrics)
+
+    predictions, labels, metrics = trainer.predict(dataset["test"], metric_key_prefix="predict")
+    trainer.log_metrics("predict", metrics)
+    trainer.save_metrics("predict", metrics)
+
 
 if __name__ == "__main__":
     main()
