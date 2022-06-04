d4d7eb381a22ceddf871b5000fff4b424da31040
jgolde <jonas.golde@gmail.com>
2022-06-04 14:26:08 +0200

    add zero-shot to baseline

--- run_kshot.py
+++ run_kshot.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
     parser.add_argument("--lr", type=float, default=5e-6)
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--seed", type=int, default=10)
-    parser.add_argument("--k", type=list, default=[1,2,4,8,16,32,64])
+    parser.add_argument("--k", type=list, default=[0,1,2,4,8,16,32,64])
     args = parser.parse_args()
 
     main(args)
